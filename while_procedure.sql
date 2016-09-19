b676b39dbc6682ee6fb75b0de4582cf21c50d0cd
Kuziv Alexander <makklays@gmail.com>
2016-09-19 14:46:00 +0300

    Create while_procedure.sql
    Example of procedure with while

---
+++ while_procedure.sql
@@ -0,0 +1,11 @@
+CREATE DEFINER = 'root'@'localhost'
+PROCEDURE while_procedure()
+BEGIN
+    DECLARE i INT;
+    SET i = 1;
+
+    WHILE i < 1000 DO
+        INSERT INTO `table` SET `val` = i;
+        SET i = i + 1;
+    END WHILE;
+END
